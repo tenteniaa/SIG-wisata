3972628296c3d2a3664485addd4e0a05115c9082
tenteniaa <teniafebrianti@students.unnes.ac.id>
2023-11-19 09:43:02 +0700

    integrasi maps

--- app/Http/Controllers/WisataController.php
+++ app/Http/Controllers/WisataController.php
@@ -10,7 +10,8 @@ class WisataController extends Controller
 {
     public function index(){
         $title = 'Landing Page';
-        $wisata = Wisata::oldest()->paginate(10)->withQueryString();
+        // $wisata = Wisata::oldest()->paginate(10)->withQueryString();
+        $wisata = Wisata::oldest()->get();
         $jumlah_wisata = Wisata::count();
 
         return view('pages.landingpage', compact('title', 'wisata', 'jumlah_wisata'));

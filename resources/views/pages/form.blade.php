2e62c92bc65f646ce84b0a0de7077396547d0a7a
alfinanora <alfinanora48@gmail.com>
2023-11-25 23:48:51 +0700

    form

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -6,7 +6,7 @@
 		<div class="">
 			<div class="page-title">
 				<div class="title_left">
-					<h3>Form Elements</h3>
+					<h3>Form Data Wisata</h3>
 				</div>
 
 				<div class="title_right">
@@ -24,7 +24,7 @@
 				<div class="col-md-12 col-sm-12 ">
 					<div class="x_panel">
 						<div class="x_title">
-							<h2>Form Data Wisata<small>different form elements</small></h2>
+							<h2>Form Data Wisata</h2>
 							<ul class="nav navbar-right panel_toolbox">
 								<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
 								</li>

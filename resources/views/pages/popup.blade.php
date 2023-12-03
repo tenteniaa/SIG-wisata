422d68e5e9725383228e4854db974f3a2014b36c
tenteniaa <teniafebrianti@students.unnes.ac.id>
2023-12-04 01:56:10 +0700

    navigasi

--- resources/views/pages/popup.blade.php
+++ resources/views/pages/popup.blade.php
@@ -10,12 +10,13 @@
             <li>Harga : Rp {{ number_format($wisata->harga, 0, ',', '.') }}</li>
             <li>{{ $wisata->region->nama_region }}, Jawa Tengah</li>
         </ul>
-        @if(request()->is('/'))
+        @if(request()->is('/') || request()->is('dashboard'))
             <a href="{{ route('wisata.detail', $wisata->id) }}" class="more_info"><i class="bi bi-link"></i> More Info</a>
         @else
-            @if($wisata->sosmed)
+            {{-- @if($wisata->sosmed)
                 <a href="{{ $wisata->sosmed }}" target="_blank" class="more_info"><i class="bi bi-link"></i> Kunjungi</a>
-            @endif
+            @endif --}}
+            <a href="#" class="more_info" onclick="navigateToCurrentLocation()"><i class="bi bi-geo-alt"></i> Navigasi</a>
         @endif
     </div>
 </div>
